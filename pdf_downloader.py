2f900ebe23e33d61be0ce92acbe0c38a51d7fc47
Matthias Lee <matthias.lee.dong.en@gmail.com>
2021-03-01 18:17:11 +0800

    updates

--- pdf_downloader.py
+++ pdf_downloader.py
@@ -5,6 +5,9 @@ from urllib.parse import urlparse
 import wget
 from urllib.request import urlopen
 import urllib.request 
+import pandas as pd
+
+data = pd.read_excel('test_papers.xlsx')
 
 hdrs = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -14,31 +17,33 @@ hdrs = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML
        'Connection': 'keep-alive'}
 
 def get_pdfs(my_url):
-    links = []
-    filenames = []
-    html = requests.get(my_url).text
-    html_page = bs(html, features="lxml")
-    base = urlparse(my_url)
-    print("base",base)
-    for link in html_page.find_all('a'):
-        current_link = link.get('href')
-        if current_link.endswith('pdf'):
-            filenames.append(current_link.rsplit('/',1)[1])
-            links.append(base.scheme + "://" + base.netloc + "/" + current_link)
-            print(links)
-            print(filenames)
-            
-    for link in links:
-        i=0
-        r = requests.get(link)
-        with open (str(filenames[i]),'wb') as f:
-             f.write(r.content)
-        i+=1
+    for url in my_url:
+        links = []
+        filenames = []
+        html = requests.get(url).text
+        html_page = bs(html, features="lxml")
+        base = urlparse(url)
+        print("base",base)
+        for link in html_page.find_all('a'):
+            current_link = link.get('href')
+            if current_link.endswith('pdf'):
+                filenames.append(current_link.rsplit('/',1)[1])
+                links.append(base.scheme + "://" + base.netloc + "/" + current_link)
+                print(links)
+                print(filenames)
+                
+        for link in links:
+            i=0
+            r = requests.get(link)
+            with open (str(filenames[i]),'wb') as f:
+                 f.write(r.content)
+            i+=1
 
 
 def main():
     print("Enter Link: ")
-    my_url = input()
+    my_url = data.link.tolist()
     get_pdfs(my_url)
 
 main()
+
